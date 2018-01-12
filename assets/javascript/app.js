c5fe09995c84b4111189296044dba895f439b0ef
anishbnair <nbanish@gmail.com>
2018-01-12 15:33:40 -0500

    included a function to display gif images when user click on each legend button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ var legendList = ["Federer", "Messi", "Kobe", "Ronaldo"];
 // Functton to render buttons as per list of items in the array
 function renderButtons() {
     $("#legendButtons").empty();
-    // Create a button for each item in the array list 
+    // Create a button for each item in the array list
     for (var i = 0; i < legendList.length; i++) {
         var tempButton = $("<button>");
         tempButton.addClass("legendButton btn btn-success");
@@ -15,6 +15,41 @@ function renderButtons() {
         $("#legendButtons").append(tempButton);
     }
 }
+
+// Function to display gifs when user click on each legend button
+function displayGifs() {
+    var legendName = $(this).attr("data-name");
+    var apiKey = "dc6zaTOxFJmzC";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + legendName + "&api_key=" + apiKey + "&rating=pg&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (queryResponse) {
+
+        var results = queryResponse.data;
+
+        $("#displayGifs").empty();
+
+        for (var i = 0; i < results.length; i++) {
+            var legendDiv = $("<div class='legend-div'>");
+            var ratingInfo = $("<p>").text("Rating: " + results[i].rating);
+            var legendGif = $("<img>");
+            legendGif.addClass("legendGif");
+
+            legendGif.attr("src", results[i].images.downsized_still.url);
+            legendGif.attr("data-still", results[i].images.downsized_still.url);
+            legendGif.attr("data-animate", results[i].images.downsized.url);
+            legendGif.attr("data-state", "still");
+            legendDiv.append(ratingInfo);
+            legendDiv.append(legendGif);
+            $("#displayGifs").append(legendDiv);
+        }
+    })
+}
+
+// Call DiplayGifs function when user click on legend name button
+$(document).on("click", ".legendButton", displayGifs);
 
 $(document).ready(function () {
     // Call renderButtons function
