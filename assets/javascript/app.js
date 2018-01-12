f81ceeb325680dd36d24637fe621d58e911662d5
anishbnair <nbanish@gmail.com>
2018-01-12 15:21:37 -0500

    included a function to render buttons as per the list of items in the array when the page loads

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,22 @@
 
-// Array to store legends
-var legendArray = [];
 
+// Array to hold legend names
+var legendList = ["Federer", "Messi", "Kobe", "Ronaldo"];
 
-// Function to get Gifs
-function getGifs() {
-    // Store user input to a variable
-    var userInput = $("#InputName").val().trim();
-    console.log(userInput);
-    var apiKey = "dc6zaTOxFJmzC";
-    var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + userInput + "&limit=10";
-    console.log(queryUrl);
-
-    $.ajax({
-        url: queryUrl,
-        Method: "GET"
-    }).done(function (queryResponse) {
-        var gifs = queryResponse.data;
-        $("#displayGifs").empty();
-        for (var i = 0; i < gifs.length; i++) {
-            var url = gifs[i].images.downsized.url;
-            $("#displayGifs").append($("<img>").attr("src", url));
-        }
-    })
+// Functton to render buttons as per list of items in the array
+function renderButtons() {
+    $("#legendButtons").empty();
+    // Create a button for each item in the array list 
+    for (var i = 0; i < legendList.length; i++) {
+        var tempButton = $("<button>");
+        tempButton.addClass("legendButton btn btn-success");
+        tempButton.attr("data-name", legendList[i]);
+        tempButton.text(legendList[i]);
+        $("#legendButtons").append(tempButton);
+    }
 }
 
-$("#addLegend").on("click", getGifs);
+$(document).ready(function () {
+    // Call renderButtons function
+    renderButtons();
+})
