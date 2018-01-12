2eaa0aad2b3aada74dbe939a83c029374b4be3c9
anishbnair <nbanish@gmail.com>
2018-01-12 15:54:52 -0500

    click event function to animate gifs or make it still when user click on images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -70,6 +70,20 @@ $("#addLegend").on("click", function (event) {
     }
 })
 
+// Click event function to animate gifs or make it still when user click on images
+$(document).on("click", ".legendGif", function () {
+
+    var state = $(this).attr("data-state");
+
+    if (state == "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
+})
+
 // Call DiplayGifs function when user click on legend name button
 $(document).on("click", ".legendButton", displayGifs);
 
